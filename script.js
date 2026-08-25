a81e006c660f1e2c29ab82b027415a2cac6c04d5
BeeyondMeanings <maduabuchi07@gmail.com>
2026-08-25 11:03:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,194 @@
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monterHealth;
+let inventory = {"stick"};
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+const weapons = [
+   {
+      name: "stick",
+      power: 5
+   },
+   {
+      name: "dagger",
+      power: 30
+   },
+   {
+      name: "claw hammer",
+      power: 50
+   },
+   {
+      name: "sword", 
+      power: 100
+   }
+];
+
+const monsters = [
+   {
+      name: "slime",
+      level: 2,
+      health: 15
+   },
+   {
+      name: "fanged beast",
+      level: 8,
+      health: 60
+   },
+   {
+      name: "dragon",
+      level: 20,
+      health: 300
+   }
+];
+
+const locations = [
+   {
+      name:"town square",
+      "button text": ["Go to store", "Go to cave", "fight dragon"],
+      "button functions": [goStore, goCave, fightDragon],
+      text: "You are in the town square. You see a sign that says \"store.\""
+},
+{
+   name: "store",
+   "button text": ["Buy 10 health (10 gold)", "Buy weapon(30 gold)", "Go to town square"],
+   "button functions": [buyHealth, buyWeapon, goTown],
+   text: "You enter the store."
+
+}  
+{
+   name: "cave", 
+   "button text": ["fightSlime", "fight fanged beast", "Go to town square" ],
+   "button functions": [fightSlime, fightBeast, goTown],
+   text: "You enter the cave. You see some monsters."
+}
+   }
+
+
+
+]
+
+// This is a single line comment in script.js
+
+/*
+This is 
+multi line Comment
+in ScriptProcessorNode.js
+*/
+
+// initialise buttons
+
+button1.onClick = goStore;
+button2.onClick = goCave;
+button3.onClick = fightDragon;
+
+function update(location) {
+      button1.innerText = location["button text"][0];
+   button2.innerText = location["button text"][1];
+   button3.innerText = location["button text"][2];
+   button1.onClick = location["button functions"][0];
+   button2.onClick = location["button functions"][1];
+   button3.onClick = location["button functions"][2];
+   text.innerText = location.text;
+
+}
+
+function goTown(){
+   update(locations[0]);
+
+}
+
+function goStore() {
+   update(locations[1])
+  
+    
+}
+
+function goCave() {
+   console.log("Going to cave.")
+}
+
+function fightDragon() {
+   console.log("fighting dragon.")
+}
+
+function buyHealth() {
+   if (gold >= 10) {
+   gold -= 10
+   health += 10
+   goldText.innerText = gold;
+   healthText.innerText = health;
+   } else {
+      text.innerText = "You do not have enough gold to buy health.";
+   }
+
+}
+
+function buyWeapon(){}
+ if (currentWeapon < weapons.length -1){
+   if (gold >= 30){
+      gold -+ 30;
+      currentWeapon += 1;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a" + newWeapon + "*";
+      inventory.push(newWeapon);
+      text.innerText += "In your inventory you have: " + inventory;
+   }else {
+      text.innerText = "You do not have enough gold to buy a weapon. ";
+      button2.innerText = "sell weapon for 15 gold";
+      button2.onClick = sellweapon;
+   }
+
+}
+
+function sellWeapon(){
+   if (inventory.length > 1){
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + "*";
+      text.innerText += "In your inventory you have: " + inventory;
+   } else {
+      text.innerText = "Don't sell your only weapon!";
+
+   }
+}
+
+
+function dodge(){
+   text.innerText = " You dodge the attack from the " + monsters[fighting].name + ".";
+
+
+}
+
+function defeatMonster(){
+   gold += Math.floor(monsters[fighting].level * 6.7
+   xp += monsters[fighting].level;
+   goldText.innerText = gold;
+   xpText.innerText = xp;
+   update(locations[4])
+}
+
+function fightDragon() {
+   fighting = 2;
+   goFight();
+}
+
+function goFight() {
+    update(Locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsteerStats.style.display = "block";
+
+}
